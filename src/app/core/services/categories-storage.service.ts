17cff8d4c90c0c82f2d115f42d2e926a3d875f90
x404 <kronos2003@gmail.com>
2024-12-25 16:38:18 +0200

    feat: create data Categories service

---
+++ src/app/core/services/categories-storage.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Category } from "@interface/interfaces";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CategoriesStorageService {
+    private categories: Category[] = [
+        {
+            "id": 1,
+            "name": "Groceries"
+        },
+        {
+            "id": 2,
+            "name": "Salary"
+        },
+        {
+            "id": 3,
+            "name": "Entertainment"
+        }
+    ]
+
+    getCategories = () => {
+        return this.categories;
+    }
+
+}
